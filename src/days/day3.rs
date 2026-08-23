ce0d807dc06acce0c6f9ce637a6b47d451827499
Michael Scinocca <mscinocca@gmail.com>
2026-08-23 14:45:18 -0400

    feat: Day 3 Part 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -20,7 +20,48 @@ pub fn part1() {
     println!("{valid_count}");
 }
 
-pub fn part2() {}
+pub fn part2() {
+    let input = read_to_string("data/day3.txt").unwrap();
+
+    let mut valid_count = 0;
+
+    let mut triangles = Vec::new();
+
+    let mut triangle1 = Vec::new();
+    let mut triangle2 = Vec::new();
+    let mut triangle3 = Vec::new();
+
+    for line in input.lines().filter(|l| !l.is_empty()) {
+        let sides: Vec<_> = line
+            .split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.trim().parse::<u32>().unwrap())
+            .collect();
+
+        triangle1.push(sides[0]);
+        triangle2.push(sides[1]);
+        triangle3.push(sides[2]);
+
+        if triangle1.len() == 3 {
+            triangles.push(triangle1.clone());
+            triangle1.clear();
+
+            triangles.push(triangle2.clone());
+            triangle2.clear();
+
+            triangles.push(triangle3.clone());
+            triangle3.clear();
+        }
+    }
+
+    for triangle in triangles {
+        if is_valid_triangle(triangle[0], triangle[1], triangle[2]) {
+            valid_count += 1;
+        }
+    }
+
+    println!("{valid_count}");
+}
 
 fn is_valid_triangle(side_1: u32, side_2: u32, side_3: u32) -> bool {
     side_1 + side_2 > side_3 && side_1 + side_3 > side_2 && side_2 + side_3 > side_1
