e544ed661166753b7b6154838d0fd32845e924d9
Michael Scinocca <mscinocca@gmail.com>
2026-09-20 19:27:18 -0400

    feat: Day 19 (slow) part 2

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -1,32 +1,21 @@
 pub fn part1() {
-    let mut elves = [1; 3005290].to_vec();
+    let mut elves: Vec<_> = (1..=3005290).map(|i| (i as u32, 1_u8)).collect();
 
-    let elf = 'outer: loop {
+    let elf = loop {
         for elf in 0..elves.len() {
-            if elves[elf] == 0 {
+            if elves[elf].1 == 0 {
                 continue;
             }
 
-            let mut next_elf = elf + 1;
+            let next_elf = { if elf >= elves.len() - 1 { 0 } else { elf + 1 } };
 
-            loop {
-                if next_elf >= elves.len() {
-                    next_elf = 0;
-                }
+            elves[next_elf].1 = 0;
+        }
 
-                if next_elf == elf {
-                    break 'outer elf + 1;
-                }
+        elves.retain(|e| e.1 != 0);
 
-                if elves[next_elf] != 0 {
-                    break;
-                }
-
-                next_elf += 1;
-            }
-
-            elves[elf] += elves[next_elf];
-            elves[next_elf] = 0;
+        if elves.len() == 1 {
+            break elves[0].0;
         }
     };
 
@@ -34,5 +23,35 @@ pub fn part1() {
 }
 
 pub fn part2() {
-    println!("Day 19 Part 2");
+    let mut elves: Vec<_> = (1..=3005290).collect();
+
+    let mut elf = 0;
+
+    let elf = loop {
+        let mut next_elf = elf + elves.len() / 2;
+
+        if next_elf >= elves.len() {
+            next_elf -= elves.len();
+        }
+
+        elves.remove(next_elf);
+
+        if elves.len() == 1 {
+            break elves[0];
+        }
+
+        if elf == elves.len() - 1 {
+            continue;
+        }
+
+        if next_elf > elf {
+            elf += 1;
+        }
+
+        if elf >= elves.len() {
+            elf = 0;
+        }
+    };
+
+    println!("{}", elf);
 }
